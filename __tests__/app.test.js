5e6c176b45255bc888587f2fd1907898339491ab
Michael Rider <mikepdxrider@gmail.com>
2021-10-28 15:53:56 -0700

    Initial commit

---
+++ __tests__/app.test.js
@@ -0,0 +1,6 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('app routes', () => {
+
+});
---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./lib/app');
+
+app.listen(7890, () => {
+  console.log('Application started!');
+});
